b37493d0555721f0398dc55cb4845cf8392e5a89
yash98 <yashmalviya98@gmail.com>
2018-09-16 15:36:49 +0530

    add new function change DS of Board

--- src/Board.h
+++ src/Board.h
@@ -4,17 +4,23 @@
 #include "Point.h"
 
 #include <vector>
+#include <tuple>
 
 class Board {
 public:
     /* FIELDS */
 
     // 3 axes to denote 2D plane (ease of co-olinear access) David Peter coordinate system
-    std::vector<std::vector<std::vector<Point>>> triLinearBoard;
+    std::vector<std::vector<Point>> triLinearBoard;
 
     /* FUNCTIONS */
 
     Board(int boardSize);
+
+    // gets the point
+    // give cordinate x = [-boardSize, boardSize], y = [-boardSize, boardSize]
+    // 5 ring game then boardSize = 5 
+    Point getPointTriLinear(std::tuple<int, int> triLinearCoordinate);
 };
 
 #endif /* BOARD_H */
