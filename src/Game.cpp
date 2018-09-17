7536f363633fbfb479653c77913ce81b1dc468eb
yash98 <yashmalviya98@gmail.com>
2018-09-17 22:49:01 +0530

    add contiguous sequence function precheck

---
+++ src/Game.cpp
@@ -0,0 +1,133 @@
+#include "Game.h"
+#include "Player.h"
+#include "Board.h"
+#include "Point.h"
+#include "Move.h"
+#include "Operation.h"
+
+
+#include <vector>
+#include <tuple>
+
+Game::Game(int ringAtStart) {
+    phase = placement;
+    chance = orange;
+    ringNum = ringAtStart;
+    board = Board(ringAtStart);
+    playerTuple = std::make_tuple(Player(ringAtStart), Player(ringAtStart));
+}
+
+void Game::checkAndAddContigous(int directionX, int directionY, int i, int j, 
+bool& seenSameColorMarker, std::vector<Operation>& tempOpQueue, 
+int& numOfContiguous, Operation& rowStart, Operation& rowEnd, std::vector<Move>& movesToReturn,
+int contiguousNum) {
+    Point tempPoint = board.getPointTriLinear(i, j);
+
+            // seen same color marker
+            if ((tempPoint.color == chance) && (tempPoint.piece == Point::marker)) {
+                if (!seenSameColorMarker) {
+                    tempOpQueue.clear();
+                    rowStart = Operation(Operation::RS, i, j);
+                }
+                seenSameColorMarker = true;
+                numOfContiguous++;
+                if (numOfContiguous >= contiguousNum-1) {
+                    rowEnd = Operation(Operation::RE, i, j);
+                    tempOpQueue.push_back(rowStart);
+                    tempOpQueue.push_back(rowEnd);
+                    Move tempMoveStore = Move(tempOpQueue);
+                    movesToReturn.push_back(tempMoveStore);
+                    int tempCoordX = std::get<0>(rowStart.coordinate);
+                    int tempCoordY = std::get<1>(rowStart.coordinate);
+                    rowStart = Operation(Operation::RS, tempCoordX-directionX, tempCoordY-directionY);
+                }
+            } else {
+                numOfContiguous = 0;
+                seenSameColorMarker = false;
+            }
+}
+
+std::vector<Move> Game::contiguousMarker(int contiguousNum) {
+    // loop constraint variables
+    std::vector<Move> movesToReturn;
+    int jMin;
+    int jMax;
+    int iMin;
+    int iMax;
+    bool seenSameColorMarker;
+    int numOfContiguous;
+    Operation rowStart;
+    Operation rowEnd;
+    std::vector<Operation> tempOpQueue;
+    // +(0, 1) direction
+    for (int i=-board.boardSize; i<=board.boardSize; i++) {
+        tempOpQueue.clear();
+        numOfContiguous = 0;
+        seenSameColorMarker = false;
+
+        // see only majority of existent
+        if (i<=0) {
+            jMax = i+board.boardSize;
+            jMin = -board.boardSize;
+        } else {
+            jMin = i-board.boardSize;
+            jMax = board.boardSize;
+        }
+        for (int j=jMin; j<=jMax; j++) {
+            checkAndAddContigous(0, 1, i, j, 
+            seenSameColorMarker, tempOpQueue, 
+            numOfContiguous, rowStart, rowEnd, movesToReturn,
+            contiguousNum);
+        }
+    }
+
+    // +(1, 0) direction
+    for (int j=-board.boardSize; j<=board.boardSize; j++) {
+        tempOpQueue.clear();
+        numOfContiguous = 0;
+        seenSameColorMarker = false;
+        if (j<=0) {
+            iMax = j+board.boardSize;
+            iMin = -board.boardSize;
+        } else {
+            iMin = j-board.boardSize;
+            iMax = board.boardSize;
+        }
+        for (int i=iMin; i<=iMax; i++) {
+            checkAndAddContigous(1, 0, i, j, 
+            seenSameColorMarker, tempOpQueue, 
+            numOfContiguous, rowStart, rowEnd, movesToReturn,
+            contiguousNum);
+        }
+    }
+
+    // +(1, 1) direction
+    int i;
+    int j;
+    // y = -boardSize starting points
+    for (iMin = -board.boardSize; iMin <= 0; iMin++) {
+        tempOpQueue.clear();
+        numOfContiguous = 0;
+        seenSameColorMarker = false;
+        for (i = iMin, j = -board.boardSize; (i <= board.boardSize) && (j <= board.boardSize); i++, j++) {
+            checkAndAddContigous(1, 1, i, j, 
+            seenSameColorMarker, tempOpQueue, 
+            numOfContiguous, rowStart, rowEnd, movesToReturn,
+            contiguousNum);
+        }
+    }
+
+    // x = -boardSize starting points
+    for (jMin = -board.boardSize; jMin <= 0; iMin++) {
+        tempOpQueue.clear();
+        numOfContiguous = 0;
+        seenSameColorMarker = false;
+        for (i = -board.boardSize, j = jMin; (i <= board.boardSize) && (j <= board.boardSize); i++, j++) {
+            checkAndAddContigous(1, 1, i, j, 
+            seenSameColorMarker, tempOpQueue, 
+            numOfContiguous, rowStart, rowEnd, movesToReturn,
+            contiguousNum);
+        }
+    }
+    return movesToReturn;
+}
--- src/Game.h
+++ src/Game.h
@@ -16,6 +16,12 @@ public:
     Board board;
     // Number of rings each player has at start of game. Board coordinate range = (-ringNum, ringNum)
     int ringNum;
+    // Game phase: Placement, Movement
+    enum phaseType {placement, movement};
+    phaseType phase;
+
+    enum chanceType {orange, blue};
+    chanceType chance;
 
     // players in game. 0 moves first.
     std::tuple<Player, Player> playerTuple;
@@ -24,13 +30,18 @@ public:
     std::vector<std::vector<Move>> currentPlayerPossibleMovesAllRings;
 
     /* FUNCTIONS */
-    Game();
+    // TODO: defualt = 5.
+    Game(int ringAtStart = 5);
 
-    std::vector<Move> possibleMovesForRingInDirection(Point ring, std::tuple<int, int> direction);
+    // find contigous (contiguousNum) # of markers in each linear direction
+    // return something like RS (0, -2) RE (4, 2)
+    std::vector<Move> contiguousMarker(int contiguousNum);
+
+    std::vector<Move> possibleMovementForRingInDirection(Point ring, std::tuple<int, int> direction);
     // Moves() parses HexCoord in string format and gives it to this function
-    std::vector<Move> possibleMovesForRingAllDirection(Point ring);
+    std::vector<Move> possibleMovementForRingAllDirection(Point ring);
     // find all possible moves for rings of current player
-    std::vector<std::vector<Move>> possibleMovesAllRingAllDirection(Player currentPlayer);
+    std::vector<std::vector<Move>> possibleMovementAllRingAllDirection(Player currentPlayer);
 
     // changes board configuration according to move(if valid)
     void ExecuteMove(Move m);
@@ -39,6 +50,13 @@ public:
     // checks validity of move before it is execute.
     // searchs moves in currentPlayerPossibleMovesAllRings array
     bool checkMoveValidity(Move m);
+
+
+    // repetitive part for contiguousMarker
+    void checkAndAddContigous(int directionX, int directionY, int i, int j, 
+    bool& seenSameColorMarker, std::vector<Operation>& tempOpQueue, 
+    int& numOfContiguous, Operation& rowStart, Operation& rowEnd, std::vector<Move>& movesToReturn,
+    int contiguousNum);
 };
 
 #endif /* GAME_H */
