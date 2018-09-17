464419cdad4844764d531cace60b616a2af70c82
sanket hire <sankethire98@gmail.com>
2018-09-17 22:45:33 +0530

    add copy constructor test case for board

--- testCase/Board.cpp
+++ testCase/Board.cpp
@@ -6,3 +6,17 @@ int main(){
     return 0;
 }
 
+int main(){
+    Point::defineTrilinearDirection();
+    Board board = Board(5);
+    Board copyboard = board;
+    std::tuple<int, int> p = std::make_tuple(4,2);
+    board.setPointTriLinear(p,Point::ring,Point::orange);
+    Point po = board.getPointTriLinear(p);
+    std::cout << po.piece << " " << po.color << std::endl; 
+    
+    std::cout << board.toStringBoard();
+    std::cout << std::endl;
+    std::cout << copyboard.toStringBoard();
+    return 0;
+}
