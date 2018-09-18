a85b483e4ec4a053c2af8e1f8150505fc35fbbbe
yash98 <yashmalviya98@gmail.com>
2018-09-18 21:27:32 +0530

    test case contiguousMarker

--- testCase/Game.cpp
+++ testCase/Game.cpp
@@ -2,6 +2,8 @@ int main(){
     Point::defineTrilinearDirection();
     Game game = Game(5); 
 
+    std::tuple<int, int> p0 = std::make_tuple(0,4);
+    game.board.setPointTriLinear(p0,Point::marker,Point::orange);
     std::tuple<int, int> p1 = std::make_tuple(1,4);
     game.board.setPointTriLinear(p1,Point::marker,Point::orange);
     std::tuple<int, int> p2 = std::make_tuple(2,4);
@@ -13,17 +15,42 @@ int main(){
     std::tuple<int, int> p5 = std::make_tuple(5,4);
     game.board.setPointTriLinear(p5,Point::marker,Point::orange);
 
-    std::vector<Move> rem_move = game.contiguousMarker(5);
+    // std::tuple<int, int> p0 = std::make_tuple(-2,-2);
+    // game.board.setPointTriLinear(p0,Point::marker,Point::orange);
+    // std::tuple<int, int> p1 = std::make_tuple(-1,-1);
+    // game.board.setPointTriLinear(p1,Point::marker,Point::orange);
+    // std::tuple<int, int> p2 = std::make_tuple(0,0);
+    // game.board.setPointTriLinear(p2,Point::marker,Point::orange);
+    // std::tuple<int, int> p3 = std::make_tuple(1,1);
+    // game.board.setPointTriLinear(p3,Point::marker,Point::orange);
+    // std::tuple<int, int> p4 = std::make_tuple(2,2);
+    // game.board.setPointTriLinear(p4,Point::marker,Point::orange);
+    // std::tuple<int, int> p5 = std::make_tuple(3,3);
+    // game.board.setPointTriLinear(p5,Point::marker,Point::orange);
+
+    p0 = std::make_tuple(-2,-2);
+    game.board.setPointTriLinear(p0,Point::marker,Point::orange);
+    p1 = std::make_tuple(-1,-1);
+    game.board.setPointTriLinear(p1,Point::marker,Point::orange);
+    p2 = std::make_tuple(0,0);
+    game.board.setPointTriLinear(p2,Point::marker,Point::orange);
+    p3 = std::make_tuple(1,1);
+    game.board.setPointTriLinear(p3,Point::marker,Point::orange);
+    p4 = std::make_tuple(2,2);
+    game.board.setPointTriLinear(p4,Point::marker,Point::orange);
+    // p5 = std::make_tuple(3,3);
+    // game.board.setPointTriLinear(p5,Point::marker,Point::orange);
+
+    std::vector<Move> rem_move = game.contiguousMarker(5, Game::orange);
 
 
     std::cout << game.board.Board::toStringBoard();
 
     for(int i=0; i< rem_move.size();i++){
         for(int j=0;j<rem_move[i].operationSequence.size();j++){
-            std::cout << rem_move[i].operationSequence[j].opcode << " " 
-            << std::get<0>(rem_move[i].operationSequence[j].coordinate) << " " 
+            std::cout << rem_move[i].operationSequence[j].opcode << ", " 
+            << std::get<0>(rem_move[i].operationSequence[j].coordinate) << ", " 
             << std::get<1>(rem_move[i].operationSequence[j].coordinate) << std::endl;
-
         }
     }
 
