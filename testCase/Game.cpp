1713b507ad671d44971f498ed6ad23c25bc69c84
yash98 <yashmalviya98@gmail.com>
2018-09-20 06:48:11 +0530

    add test case for possibleMovements and comments

--- testCase/Game.cpp
+++ testCase/Game.cpp
@@ -1,3 +1,4 @@
+// contiguous markers check (Board print get set points)
 int main(){
     Point::defineTrilinearDirection();
     Game game = Game(5); 
@@ -56,5 +57,58 @@ int main(){
 
 
 
+    return 0;
+}
+
+// possibleMovements check (Player addRing)
+int main(){
+    Point::defineTrilinearDirection();
+    Game game = Game(5);
+    Player requiredPlayer = *(std::get<0>(game.playerTuple));
+
+    std::tuple<int, int> p0 = std::make_tuple(0,4);
+    game.board.setPointTriLinear(p0,Point::marker,Point::orange);
+    std::tuple<int, int> p1 = std::make_tuple(1,4);
+    game.board.setPointTriLinear(p1,Point::marker,Point::orange);
+    std::tuple<int, int> p2 = std::make_tuple(2,4);
+    game.board.setPointTriLinear(p2,Point::marker,Point::orange);
+    std::tuple<int, int> p3 = std::make_tuple(3,4);
+    game.board.setPointTriLinear(p3,Point::ring,Point::orange);
+    requiredPlayer.addRing(3, 4);
+    std::tuple<int, int> p4 = std::make_tuple(4,4);
+    game.board.setPointTriLinear(p4,Point::ring,Point::orange);
+    requiredPlayer.addRing(4, 4);
+    std::tuple<int, int> p5 = std::make_tuple(5,4);
+    game.board.setPointTriLinear(p5,Point::marker,Point::orange);
+
+    p0 = std::make_tuple(-2,-2);
+    game.board.setPointTriLinear(p0,Point::marker,Point::orange);
+    p1 = std::make_tuple(-1,-1);
+    game.board.setPointTriLinear(p1,Point::marker,Point::orange);
+    p2 = std::make_tuple(0,0);
+    game.board.setPointTriLinear(p2,Point::marker,Point::orange);
+    p3 = std::make_tuple(1,1);
+    game.board.setPointTriLinear(p3,Point::marker,Point::orange);
+    p4 = std::make_tuple(2,2);
+    game.board.setPointTriLinear(p4,Point::marker,Point::orange);
+    // p5 = std::make_tuple(3,3);
+    // game.board.setPointTriLinear(p5,Point::marker,Point::orange);
+
+    std::cout << game.board.Board::toStringBoard();
+    std::vector<std::vector<std::vector<Move>>> allPosMovements
+    = game.possibleMovementAllRingAllDirection(requiredPlayer);
+
+    int count;
+    for (std::vector<std::vector<Move>> i: allPosMovements) {
+        count = 0;
+        for (std::vector<Move> j: i) {
+            std::cout << std::get<0>(triLinearDirection[count]) << ", " 
+            << std::get<1>(triLinearDirection[count]) << std::endl;
+            for (Move k: j) {
+                std::cout << k.toStr();
+            }
+            count++;
+        }
+    }
     return 0;
 }
