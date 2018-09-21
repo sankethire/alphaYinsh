6038b39e729f738a2460b252cecd5b629b7007f9
sanket hire <sankethire98@gmail.com>
2018-09-21 11:52:44 +0530

    david peter yinsh colouring

--- testCase/Point.cpp
+++ testCase/Point.cpp
@@ -9,3 +9,27 @@ int main(){
     
     return 0;
 }
+
+// David peter colors
+std::string Point::toColoredStringPoint() {
+    if (piece == Point::ring) {
+        if (color == Point::orange) {
+            return "\033[0;34mO\033[0m";
+        } else if (color == Point::blue) {
+            return "\033[0;32mO\033[0m";
+        }
+    } else if (piece == Point::marker) {
+        if (color == Point::orange) {
+            return "\033[0;44m.\033[0m";
+        } else if (color == Point::blue) {
+            return "\033[0;42m.\033[0m";
+        }
+    } else if (piece == Point::emptyPiece) {
+        return "*";
+    } else {
+        return "";
+    }
+    std::stringstream ss;
+    ss << std::to_string(piece) << ", " << std::to_string(color);
+    return ss.str();
+}
