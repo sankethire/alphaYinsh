2d60d5ae502ed2ba7931c6fe5664ab4f064fdb4a
sanket hire <sankethire98@gmail.com>
2018-09-23 12:07:06 +0530

    testcase on check for move as hexCoord or triLinearCoord as bool

--- testCase/Move.cpp
+++ testCase/Move.cpp
@@ -8,3 +8,18 @@ int main()
 	std::cout<< move.toStr();
 	return 0;
 }
+
+// checking for move as hexCoord or triLinearCoord
+int main(){
+    Point::defineTriLinearDirection();
+    std::string s;
+    s = "P 1 2";
+    Move move = Move(s,true);
+    std::cout << move.toStr(false);
+
+    s = "P 1 0";
+    Move m = Move(s,false);
+    std::cout << m.toStr(false);
+
+    return 0;
+}
